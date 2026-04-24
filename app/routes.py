0bdc70e8fe3aed52b5e6188ba4ac83142311afd1
Richard Ryan Marroquin <marroquinryan@yahoo.com>
2026-04-24 13:25:39 -0700

    Testing Jinja Templates

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
+from flask import render_template
 
 main_bp = Blueprint('main',__name__)
 
 @main_bp.route('/')
 def index():
-    return "HololiveOCGWEbBinder running"
+    return render_template('base.html.jinja')
