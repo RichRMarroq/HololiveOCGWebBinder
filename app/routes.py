8b364a69a37a5e722a7fd41669a947dbf24916b6
Richard Ryan Marroquin <marroquinryan@yahoo.com>
2026-04-24 14:34:22 -0700

    Utilizing Jinja Templates

--- app/routes.py
+++ app/routes.py
@@ -3,6 +3,29 @@ from flask import render_template
 
 main_bp = Blueprint('main',__name__)
 
+NAVIGATION = [
+    {"href": "/", "caption": "Home"},
+    {"href": "/decks", "caption": "Decks"},
+    {"href": "/cards", "caption": "Cards"},
+    {"href": "/about", "caption": "About"}
+]
+
 @main_bp.route('/')
 def index():
-    return render_template('base.html.jinja')
+    pageName = "Home"
+    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+
+@main_bp.route('/decks')
+def decks():
+    pageName = "Decks"
+    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+
+@main_bp.route('/cards')
+def cards():
+    pageName = "Cards"
+    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+
+@main_bp.route('/about')
+def about():
+    pageName = "About"
+    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
