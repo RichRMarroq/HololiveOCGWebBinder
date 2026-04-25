64b066ccbc8c0da37103118d37afee2c7524da04
Richard Ryan Marroquin <marroquinryan@yahoo.com>
2026-04-24 23:16:23 -0700

    Concept page for Home, using Jinja and Bootstrap

--- app/routes.py
+++ app/routes.py
@@ -3,29 +3,18 @@ from flask import render_template
 
 main_bp = Blueprint('main',__name__)
 
-NAVIGATION = [
-    {"href": "/", "caption": "Home"},
-    {"href": "/decks", "caption": "Decks"},
-    {"href": "/cards", "caption": "Cards"},
-    {"href": "/about", "caption": "About"}
-]
-
 @main_bp.route('/')
 def index():
-    pageName = "Home"
-    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+    return render_template('home.html.jinja')
 
 @main_bp.route('/decks')
 def decks():
-    pageName = "Decks"
-    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+    return render_template('decks.html.jinja')
 
 @main_bp.route('/cards')
 def cards():
-    pageName = "Cards"
-    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+    return render_template('cards.html.jinja')
 
 @main_bp.route('/about')
 def about():
-    pageName = "About"
-    return render_template('base.html.jinja', title=pageName, navigation=NAVIGATION, a_variable="This is a variable passed from the route to the template.")
+    return render_template('about.html.jinja')
